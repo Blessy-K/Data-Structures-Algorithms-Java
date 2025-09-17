1ae62bc5f2384542ad2d9753c4793b2a4d962c77
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-09-17 18:15:38 +0530

    Time: 2 ms (99.75%), Space: 50.7 MB (16.79%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,27 +1,75 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-    Map<Integer, Integer> freq = new HashMap<>();
+    // Map<Integer, Integer> freq = new HashMap<>();
     List<Integer> result = new ArrayList<>();
+    int e1=0,e2=0;
+    int c1=0,c2=0;
     for(int i=0;i<nums.length;i++)
     {
-        // int cnt =0;
         int num = nums[i];
-        if(freq.containsKey(num))
+        if(num == e1)
         {
-            freq.put(num,freq.get(num)+1);
+            c1++;
+        }
+        else if(num==e2)
+        {
+            c2++;
+        }
+        else if(c1==0)
+        {
+            e1=num;
+            c1=1;
+        }
+        else if(c2==0)
+        {
+            e2=num;
+            c2=1;
         }
         else
         {
-            freq.put(num,1);
+            c1--;
+            c2--;
         }
     }
-    for(Integer key: freq.keySet())
+    c1=0;
+    c2=0;
+    for(int i=0;i<nums.length;i++)
     {
-        if(freq.get(key)>nums.length/3)
+        if(nums[i]==e1)
         {
-            result.add(key);
+            c1++;
+        }
+        else if(nums[i]==e2)
+        {
+            c2++;
         }
     }
+        if(c1>nums.length/3)
+        {
+            result.add(e1);
+        }
+        if(c2>nums.length/3)
+        {
+            result.add(e2);
+        }
+    //     // int cnt =0;
+    //     int num = nums[i];
+    //     if(freq.containsKey(num))
+    //     {
+    //         freq.put(num,freq.get(num)+1);
+    //     }
+    //     else
+    //     {
+    //         freq.put(num,1);
+    //     }
+    // }
+    // for(Integer key: freq.keySet())
+    // {
+    //     if(freq.get(key)>nums.length/3)
+    //     {
+    //         result.add(key);
+    //     }
+    // }
         // for(int j=0;j<nums.length;j++)
         // {
         //     if(nums[i]==nums[j])
