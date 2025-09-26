e01db5eb98578a0c32ee651c495c5070684e43c1
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-09-26 18:26:45 +0530

    Time: 1 ms (84.58%), Space: 40.8 MB (70.17%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,33 @@
 class Solution {
     public int mySqrt(int x) {
-    if(x<2)
-    {
-        return x;
-    }    
-    int l = 1,r = x/2, ans = 0;
-    while(l<=r)
-    {
-        int mid = l+(r-l)/2;
-        if((long)mid*mid<=x)
-        {
-            ans = mid;
-            l=mid+1;
-        }
-        else
-        {
-            r = mid-1;
-        }
-    }
-    return ans;
+       if(x==0)
+       {
+        return 0;
+       }
+       long r=x;
+       while(r*r>x)
+       {
+        r=(r+x/r)/2;
+       }
+       return (int) r;
+    // if(x<2)
+    // {
+    //     return x;
+    // }    
+    // int l = 1,r = x/2, ans = 0;
+    // while(l<=r)
+    // {
+    //     int mid = l+(r-l)/2;
+    //     if((long)mid*mid<=x)
+    //     {
+    //         ans = mid;
+    //         l=mid+1;
+    //     }
+    //     else
+    //     {
+    //         r = mid-1;
+    //     }
+    // }
+    // return ans;
     }
 }
