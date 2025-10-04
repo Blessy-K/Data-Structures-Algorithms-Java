79e473fbd02dedb731c99fe4f19ddbc8f6dfa7a3
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-04 19:50:05 +0530

    Time: 1 ms (100%), Space: 46.3 MB (27.8%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -22,28 +22,95 @@ class Solution {
     //     return (mer[total/2-1]+mer[total/2])/2.0;
     // }
 
-    int total = m+n;
-    int m1=-1,m2=-1;
-    int i=0,j=0;
-    for(int count=0;count<=total/2;count++)
+    // int total = m+n;
+    // int m1=-1,m2=-1;
+    // int i=0,j=0;
+    // for(int count=0;count<=total/2;count++)
+    // {
+    //     m1=m2;
+    //     if(i!=m && (j==n || nums1[i]<= nums2[j]))
+    //     {
+    //         m2=nums1[i++];
+    //     }
+    //     else
+    //     {
+    //         m2 = nums2[j++];
+    //     }
+    // }
+    // if(total%2==1)
+    // {
+    //     return m2;
+    // }
+    // else
+    // {
+    //     return (m1+m2)/2.0;
+    // }
+
+    if(m>n)
     {
-        m1=m2;
-        if(i!=m && (j==n || nums1[i]<= nums2[j]))
+        return findMedianSortedArrays(nums2,nums1);
+    }
+    int total = m+n;
+    int l=0,h=m;
+    while(l<=h)
+    {
+        int c1=(l+h)/2;
+        int c2 = (total+1)/2-c1;
+        int le1,le2,r1,r2;
+        if(c1==0)
         {
-            m2=nums1[i++];
+            le1=Integer.MIN_VALUE;
         }
         else
         {
-            m2 = nums2[j++];
+            le1 = nums1[c1-1];
         }
-    }
-    if(total%2==1)
+        if(c1==m)
+        {
+            r1=Integer.MAX_VALUE;
+        }
+        else
+        {
+            r1 = nums1[c1];
+        }
+        if(c2==0)
+        {
+            le2 = Integer.MIN_VALUE;
+        }
+        else
+        {
+            le2 = nums2[c2-1];
+        }
+        if(c2==n)
+        {
+            r2=Integer.MAX_VALUE;
+        }
+        else
+        {
+            r2 = nums2[c2];
+        }
+        if(le1<=r2 && le2<=r1)
+        {
+            if(total%2==0)
+            {
+                int lem = Math.max(le1,le2);
+                int rm = Math.min(r1,r2);
+                return (lem+rm)/2.0;
+            }
+            else
+            {
+                return Math.max(le1,le2);
+            }
+        }
+    else if(le1>r2)
     {
-        return m2;
+        h=c1-1;
     }
     else
     {
-        return (m1+m2)/2.0;
+        l=c1+1;
     }
-    }
+}
+    return 0.0;
+}
 }
