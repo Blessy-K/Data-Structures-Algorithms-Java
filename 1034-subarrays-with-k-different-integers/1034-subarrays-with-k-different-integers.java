bce9c0f875dd8728db6c1a379b297cc40639ecf9
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-08 17:22:22 +0530

    Time: 5 ms (99.13%), Space: 47.4 MB (76.12%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -25,22 +25,27 @@ class Solution {
         //         // count++;
         //     }
         // }
-        return atMost(nums,k)-atMost(nums,k-1);
+        return count(nums,k)-count(nums,k-1);
     }
-        private static int atMost(int nums[],int k)
+        private static int count(int nums[],int k)
         {
-        int l=0,c=0;
-        Map<Integer,Integer> freq = new HashMap<>();
-        for(int r=0;r<nums.length;r++)
+            int n = nums.length;
+            int freq[] = new int[n+1];
+        int l=0,c=0,d=0;
+        // Map<Integer,Integer> freq = new HashMap<>();
+        for(int r=0;r<n;r++)
         {
-            freq.put(nums[r],freq.getOrDefault(nums[r],0)+1);
-            if(freq.get(nums[r])==1)
-            k--;
-            while(k<0)
+            // freq.put(nums[r],freq.getOrDefault(nums[r],0)+1);
+            if(freq[nums[r]]==0)
+            d++;
+            freq[nums[r]]++;
+            while(d>k)
             {
-                freq.put(nums[l],freq.get(nums[l])-1);
-                if(freq.get(nums[l])==0)
-                k++;
+                // freq.put(nums[l],freq.get(nums[l])-1);
+                // if(freq.get(nums[l])==0)
+                freq[nums[l]]--;
+                if(freq[nums[l]]==0)
+                d--;
                 l++;
             }
             c=c+(r-l+1);
