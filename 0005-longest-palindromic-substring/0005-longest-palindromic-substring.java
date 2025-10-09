458cb1cefd437a02872d1652947327f0736cc726
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-09 19:28:28 +0530

    Time: 15 ms (83.73%), Space: 42.4 MB (61.3%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -25,49 +25,49 @@ class Solution {
     //     }
     //             return true;
 
-    // if(s==null || s.length()<1)
-    // return "";
-    // int st=0,end=0;
-    // for(int i=0;i<s.length();i++)
-    // {
-    //     int l1 = expandFrameCenter(s,i,i);
-    //     int l2 = expandFrameCenter(s,i,i+1);
-    //     int l = Math.max(l1,l2);
-    //     if(l>end-st)
-    //     {
-    //         st=i-(l-1)/2;
-    //         end = i+l/2;
-    //     }
-    // }
-    // return s.substring(st,end+1);
-    // }
-    // private int expandFrameCenter(String s, int left,int right)
-    // {
-    //     while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right))
-    //     {
-    //         left--;
-    //         right++;
-    //     }
-    //     return right-left-1;
-
-    int n = s.length();
-    boolean[][] dp = new boolean[n][n];
-    String ans = "";
-    for(int len=1;len<=n;len++)
+    if(s==null || s.length()<1)
+    return "";
+    int st=0,end=0;
+    for(int i=0;i<s.length();i++)
     {
-        for(int i=0;i+len-1<n;i++)
+        int l1 = expandFrameCenter(s,i,i);
+        int l2 = expandFrameCenter(s,i,i+1);
+        int l = Math.max(l1,l2);
+        if(l>end-st)
         {
-            int j = i+len-1;
-            if(s.charAt(i)==s.charAt(j))
-            {
-                if(len<=2 || dp[i+1][j-1])
-                {
-                   dp[i][j]=true;
-                   if(len>ans.length())ans = s.substring(i,j+1);
-                }
-            }
+            st=i-(l-1)/2;
+            end = i+l/2;
         }
     }
-    return ans;
+    return s.substring(st,end+1);
+    }
+    private int expandFrameCenter(String s, int left,int right)
+    {
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right))
+        {
+            left--;
+            right++;
+        }
+        return right-left-1;
+
+    // int n = s.length();
+    // boolean[][] dp = new boolean[n][n];
+    // String ans = "";
+    // for(int len=1;len<=n;len++)
+    // {
+    //     for(int i=0;i+len-1<n;i++)
+    //     {
+    //         int j = i+len-1;
+    //         if(s.charAt(i)==s.charAt(j))
+    //         {
+    //             if(len<=2 || dp[i+1][j-1])
+    //             {
+    //                dp[i][j]=true;
+    //                if(len>ans.length())ans = s.substring(i,j+1);
+    //             }
+    //         }
+    //     }
+    // }
+    // return ans;
     }
 }
