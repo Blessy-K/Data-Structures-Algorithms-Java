d69111cb24fa69c64bec4316d3c0c6d2f94dfcfd
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-10 18:29:08 +0530

    Time: 4 ms (85.28%), Space: 45.3 MB (45.63%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -2,48 +2,64 @@ class Solution {
     public String convert(String s, int numRows) {
     if(numRows == 1 || s.length() <= numRows)
     return s;
-    char[][] matrix = new char[numRows][s.length()];
-    int row = 0,col=0;
-    boolean goingDown = true;
-    for(int i=0;i<s.length();i++)
-    {
-        matrix[row][col]=s.charAt(i);
-        if(goingDown)
-        {
-            if(row == numRows-1)
-            {
-                goingDown = false;
-                row--;
-                col++;
-            }
-            else
-            {
-                row++;
-            }
-        }
-            else
-            {
-                if(row==0)
-                {
-                    goingDown = true;
-                    row++;
-                }
-                else
-                {
-                    row--;
-                    col++;
-                }
-            }
-        }
-        StringBuilder r = new StringBuilder();
-        for(int i=0;i<numRows;i++)
-        {
-            for(int j=0;j<s.length();j++)
-            {
-                if(matrix[i][j]!=0)
-                r.append(matrix[i][j]);
-            }
-        }
-        return r.toString();   
+    // char[][] matrix = new char[numRows][s.length()];
+    // int row = 0,col=0;
+    // boolean goingDown = true;
+    // for(int i=0;i<s.length();i++)
+    // {
+    //     matrix[row][col]=s.charAt(i);
+    //     if(goingDown)
+    //     {
+    //         if(row == numRows-1)
+    //         {
+    //             goingDown = false;
+    //             row--;
+    //             col++;
+    //         }
+    //         else
+    //         {
+    //             row++;
+    //         }
+    //     }
+    //         else
+    //         {
+    //             if(row==0)
+    //             {
+    //                 goingDown = true;
+    //                 row++;
+    //             }
+    //             else
+    //             {
+    //                 row--;
+    //                 col++;
+    //             }
+    //         }
+    //     }
+    //     StringBuilder r = new StringBuilder();
+    //     for(int i=0;i<numRows;i++)
+    //     {
+    //         for(int j=0;j<s.length();j++)
+    //         {
+    //             if(matrix[i][j]!=0)
+    //             r.append(matrix[i][j]);
+    //         }
+    //     }
+    //     return r.toString();   
+
+       StringBuilder[] rows = new StringBuilder[numRows];
+       for(int i=0;i<numRows;i++)
+       rows[i]=new StringBuilder();
+       int currRow = 0;
+       boolean goingDown = false;
+       for(char c: s.toCharArray())
+       {
+        rows[currRow].append(c);
+        if(currRow==0 || currRow == numRows-1)goingDown=!goingDown;
+        currRow+=goingDown?1:-1;
+       }
+       StringBuilder r = new StringBuilder();
+       for(StringBuilder row : rows)
+       r.append(row);
+       return r.toString();
     }
 }
