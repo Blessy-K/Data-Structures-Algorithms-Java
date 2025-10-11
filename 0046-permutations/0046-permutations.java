2170c0ca9bbbcf123765da6aba144deb7937a565
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-11 10:42:33 +0530

    Time: 1 ms (93.04%), Space: 44.3 MB (93.5%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,42 @@
 class Solution {
     List<List<Integer>> r = new ArrayList<>();
-    void generate(int[] nums,List<Integer> current)
+    void backtrack(int[] nums,boolean[] used, List<Integer> current)
     {
         if(current.size()==nums.length)
         {
-            Set<Integer> set = new HashSet<>(current);
-            if(set.size()==nums.length)
             r.add(new ArrayList<>(current));
             return;
         }
-        for(int num: nums)
+        for(int i=0;i<nums.length;i++)
         {
-            current.add(num);
-            generate(nums,current);
+            if(used[i])
+            continue;
+            used[i]=true;
+            current.add(nums[i]);
+            backtrack(nums,used,current);
             current.remove(current.size()-1);
+            used[i]=false;
         }
-    } 
+    }
+    // void generate(int[] nums,List<Integer> current)
+    // {
+    //     if(current.size()==nums.length)
+    //     {
+    //         Set<Integer> set = new HashSet<>(current);
+    //         if(set.size()==nums.length)
+    //         r.add(new ArrayList<>(current));
+    //         return;
+    //     }
+    //     for(int num: nums)
+    //     {
+    //         current.add(num);
+    //         generate(nums,current);
+    //         current.remove(current.size()-1);
+    //     }
+    // } 
     public List<List<Integer>> permute(int[] nums) {   
         r.clear();
-        generate(nums,new ArrayList<>());
+        backtrack(nums,new boolean[nums.length],new ArrayList<>());
         return r;
     }
 }
