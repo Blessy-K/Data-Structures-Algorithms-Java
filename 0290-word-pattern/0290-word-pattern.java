f70fffbd6d4d252d0d1266f14be408785be39d81
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-15 18:12:32 +0530

    Time: 0 ms (100%), Space: 41.5 MB (49.94%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,24 +1,46 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-    String[] w=s.split(" ");
-    if(pattern.length()!=w.length)
+    String[] words=s.split(" ");
+    if(pattern.length()!=words.length)
     return false;
+    // for(int i=0;i<pattern.length();i++)
+    // {
+    //     for(int j=i+1;j<pattern.length();j++)
+    //     {
+    //         if(pattern.charAt(i)==pattern.charAt(j))
+    //         {
+    //             if(!w[i].equals(w[j]))
+    //             return false;
+    //         }
+    //         else
+    //         {
+    //             if(w[i].equals(w[j]))
+    //             return false;
+    //         }
+    //     }
+    // }
+    // return true;
+    // }
+
+    Map<Character,String> map = new HashMap<>();
+    Set<String> used = new HashSet<>();
     for(int i=0;i<pattern.length();i++)
     {
-        for(int j=i+1;j<pattern.length();j++)
+        char ch = pattern.charAt(i);
+        String word = words[i];
+        if(map.containsKey(ch))
         {
-            if(pattern.charAt(i)==pattern.charAt(j))
-            {
-                if(!w[i].equals(w[j]))
-                return false;
-            }
-            else
-            {
-                if(w[i].equals(w[j]))
-                return false;
-            }
+            if(!map.get(ch).equals(word))
+            return false;
+        }
+        else
+        {
+            if(used.contains(word))
+            return false;
+            map.put(ch,word);
+            used.add(word);
         }
     }
     return true;
-    }
+}
 }
