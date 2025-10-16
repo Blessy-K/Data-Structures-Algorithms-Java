722973f93eff535aa361c0950a3bca7a7a967689
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-16 18:53:01 +0530

    Time: 5 ms (54.59%), Space: 42.9 MB (94.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -11,32 +11,32 @@ class Solution {
     // return Arrays.equals(arr1,arr2);
 
 
-    // int count[]=new int[26];
-    // for(int i=0;i<s.length();i++)
-    // {
-    //     count[s.charAt(i)-'a']++;
-    //     count[t.charAt(i)-'a']--;
-    // } 
-    // for(int c : count)
-    // {
-    //     if(c!=0)
-    //         return false;
-    // }
-    // return true;
+    int count[]=new int[26];
+    for(int i=0;i<s.length();i++)
+    {
+        count[s.charAt(i)-'a']++;
+        count[t.charAt(i)-'a']--;
+    } 
+    for(int c : count)
+    {
+        if(c!=0)
+            return false;
+    }
+    return true;
 
-    HashMap<Character ,Integer> map = new HashMap<>();
-    for(char c : s.toCharArray())
-    {
-        map.put(c,map.getOrDefault(c,0)+1);
-    }
-    for(char c : t.toCharArray())
-    {
-        if(!map.containsKey(c))
-        return false;
-        map.put(c,map.get(c)-1);
-        if(map.get(c)==0)
-        map.remove(c);
-    }
-    return map.isEmpty();
+    // HashMap<Character ,Integer> map = new HashMap<>();
+    // for(char c : s.toCharArray())
+    // {
+    //     map.put(c,map.getOrDefault(c,0)+1);
+    // }
+    // for(char c : t.toCharArray())
+    // {
+    //     if(!map.containsKey(c))
+    //     return false;
+    //     map.put(c,map.get(c)-1);
+    //     if(map.get(c)==0)
+    //     map.remove(c);
+    // }
+    // return map.isEmpty();
     }
 }
