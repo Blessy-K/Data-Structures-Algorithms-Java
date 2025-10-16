2e4db6fe3eba9fa0c9f5adbdd823687c20e2f3db
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-16 18:53:29 +0530

    Time: 3 ms (90.36%), Space: 44.7 MB (43.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,25 +4,25 @@ class Solution {
     {
         return false;
     }
-    // char[] arr1 = s.toCharArray();
-    // char[] arr2 = t.toCharArray();
-    // Arrays.sort(arr1);
-    // Arrays.sort(arr2);
-    // return Arrays.equals(arr1,arr2);
+    char[] arr1 = s.toCharArray();
+    char[] arr2 = t.toCharArray();
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
+    return Arrays.equals(arr1,arr2);
 
 
-    int count[]=new int[26];
-    for(int i=0;i<s.length();i++)
-    {
-        count[s.charAt(i)-'a']++;
-        count[t.charAt(i)-'a']--;
-    } 
-    for(int c : count)
-    {
-        if(c!=0)
-            return false;
-    }
-    return true;
+    // int count[]=new int[26];
+    // for(int i=0;i<s.length();i++)
+    // {
+    //     count[s.charAt(i)-'a']++;
+    //     count[t.charAt(i)-'a']--;
+    // } 
+    // for(int c : count)
+    // {
+    //     if(c!=0)
+    //         return false;
+    // }
+    // return true;
 
     // HashMap<Character ,Integer> map = new HashMap<>();
     // for(char c : s.toCharArray())
