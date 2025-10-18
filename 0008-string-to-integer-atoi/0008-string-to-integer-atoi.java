178378093f2d58344faa8182359e7222e3eae09c
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-18 11:02:45 +0530

    Time: 1 ms (100%), Space: 42.4 MB (64.79%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,31 +1,50 @@
 class Solution {
     public int myAtoi(String s) {
-     s=s.trim();
-     if(s.length()==0)
-     {
-        return 0;
-     }   
-     int sign = 1;
-     int i=0;
-     if(s.charAt(0)=='-' || s.charAt(0)=='+')
-     {
-        sign = (s.charAt(0)=='-')?-1:1;
+    //  s=s.trim();
+    //  if(s.length()==0)
+    //  {
+    //     return 0;
+    //  }   
+    //  int sign = 1;
+    //  int i=0;
+    //  if(s.charAt(0)=='-' || s.charAt(0)=='+')
+    //  {
+    //     sign = (s.charAt(0)=='-')?-1:1;
+    //     i++;
+    //  }
+    //  long num =0;
+    //  while(i<s.length() && Character.isDigit(s.charAt(i)))
+    //  {
+    //     num = num*10+(s.charAt(i)-'0');
+    //     if(sign*num<=Integer.MIN_VALUE)
+    //     {
+    //         return Integer.MIN_VALUE;
+    //     }
+    //     if(sign*num>=Integer.MAX_VALUE)
+    //     {
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     i++;
+    //  }
+    //  return (int)(sign*num);
+
+    int i=0,n=s.length(),sign=1,num=0;
+    while(i<n && s.charAt(i)==' ')
+    i++;
+    if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-'))
+    {
+        sign = (s.charAt(i)=='-')?-1:1;
         i++;
-     }
-     long num =0;
-     while(i<s.length() && Character.isDigit(s.charAt(i)))
-     {
-        num = num*10+(s.charAt(i)-'0');
-        if(sign*num<=Integer.MIN_VALUE)
-        {
-            return Integer.MIN_VALUE;
-        }
-        if(sign*num>=Integer.MAX_VALUE)
-        {
-            return Integer.MAX_VALUE;
-        }
-        i++;
-     }
-     return (int)(sign*num);
     }
+    while(i<n && Character.isDigit(s.charAt(i)))
+    {
+        int digit = s.charAt(i)-'0';
+        if(num>(Integer.MAX_VALUE-digit)/10)
+        return (sign==1)?Integer.MAX_VALUE : Integer.MIN_VALUE;
+        num = num*10+digit;
+        i++;
+    }
+    return num*sign;
+    }
+
 }
