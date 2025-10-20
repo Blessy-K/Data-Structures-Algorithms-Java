718acaae1f18a012cb5c283bdc83a06da1e7474c
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-20 11:06:44 +0530

    Time: 1 ms (79.92%), Space: 43.4 MB (45.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,39 +1,55 @@
 class Solution {
-    List<String> result = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-    char[] current = new char[2*n];
-    generateAll(current,0);
+    List<String> result = new ArrayList<>();
+    // char[] current = new char[2*n];
+    // generateAll(current,0);
+    // return result;
+    // }
+    // private void generateAll(char[] current, int pos)
+    // {
+    //     if(pos==current.length)
+    //     {
+    //         if(isValid(current))
+    //         {
+    //             result.add(new String(current));
+    //         }
+    //     }
+    //         else
+    //         {
+    //             current[pos]='(';
+    //             generateAll(current,pos+1);
+    //             current[pos]=')';
+    //             generateAll(current,pos+1);
+    //         }
+    // }
+    //     private boolean isValid(char[] current)
+    //     {
+    //         int balance = 0;
+    //         for(char c : current)
+    //         {
+    //             if(c=='(')
+    //             balance++;
+    //             else
+    //             balance--;
+    //             if(balance<0)
+    //             return false;
+    //         }
+    //         return balance==0;
+
+
+    backtrack(result,"",0,0,n);
     return result;
-    }
-    private void generateAll(char[] current, int pos)
-    {
-        if(pos==current.length)
-        {
-            if(isValid(current))
-            {
-                result.add(new String(current));
-            }
         }
-            else
-            {
-                current[pos]='(';
-                generateAll(current,pos+1);
-                current[pos]=')';
-                generateAll(current,pos+1);
-            }
-    }
-        private boolean isValid(char[] current)
+        private void backtrack(List<String> result, String current, int open, int close, int max)
         {
-            int balance = 0;
-            for(char c : current)
+            if(current.length()==max*2)
             {
-                if(c=='(')
-                balance++;
-                else
-                balance--;
-                if(balance<0)
-                return false;
+                result.add(current);
+                return;
             }
-            return balance==0;
+            if(open<max)
+            backtrack(result,current+"(",open+1,close,max);
+            if(close<open)
+            backtrack(result,current+")",open,close+1,max);
         }
 }
