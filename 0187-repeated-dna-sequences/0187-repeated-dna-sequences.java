18f380aa6dfca67fdfddb3ca1a52576211a61edc
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-27 20:07:13 +0530

    Time: 21 ms (44.1%), Space: 49.7 MB (94.29%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -14,14 +14,37 @@ class Solution {
 
 
 
-        Map<String, Integer> freq = new HashMap<>();
-        List<String> res = new ArrayList<>();
+        // Map<String, Integer> freq = new HashMap<>();
+        // List<String> res = new ArrayList<>();
 
-        for (int i = 0; i + 9 < s.length(); i++) {
-            String sub = s.substring(i, i + 10);
-            freq.put(sub, freq.getOrDefault(sub, 0) + 1);
-            if (freq.get(sub) == 2) res.add(sub);
+        // for (int i = 0; i + 9 < s.length(); i++) {
+        //     String sub = s.substring(i, i + 10);
+        //     freq.put(sub, freq.getOrDefault(sub, 0) + 1);
+        //     if (freq.get(sub) == 2) res.add(sub);
+        // }
+        // return res;
+
+
+
+        int n = s.length();
+        if (n < 10) return new ArrayList<>();
+
+        Map<Character, Integer> map = Map.of(
+            'A', 0, 'C', 1, 'G', 2, 'T', 3);
+
+        Set<Integer> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
+        int hash = 0;
+
+        for (int i = 0; i < n; i++) {
+            hash = ((hash << 2) | map.get(s.charAt(i))) & ((1 << 20) - 1);
+
+            if (i >= 9) {
+                if (!seen.add(hash)) {
+                    repeated.add(s.substring(i - 9, i + 1));
+                }
+            }
         }
-        return res;
+        return new ArrayList<>(repeated);
     }
 }
