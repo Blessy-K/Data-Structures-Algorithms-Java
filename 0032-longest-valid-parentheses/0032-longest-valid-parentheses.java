53596e2429f0be72c7946f99cb794ebe54fdf523
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2025-10-28 20:59:52 +0530

    Time: 1 ms (99.75%), Space: 44.5 MB (81.22%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -21,19 +21,38 @@ class Solution {
     //     return balance == 0;
 
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
+        // Stack<Integer> stack = new Stack<>();
+        // stack.push(-1);
+        // int maxLen = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) == '(') {
+        //         stack.push(i);
+        //     } else {
+        //         stack.pop();
+        //         if (stack.isEmpty()) {
+        //             stack.push(i);
+        //         } else {
+        //             maxLen = Math.max(maxLen, i - stack.peek());
+        //         }
+        //     }
+        // }
+        // return maxLen;
+
+
+
+        int n = s.length();
+        int[] dp = new int[n];
         int maxLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (stack.isEmpty()) {
-                    stack.push(i);
-                } else {
-                    maxLen = Math.max(maxLen, i - stack.peek());
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] - 1 >= 0 &&
+                           s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + 2 +
+                             (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0);
                 }
+                maxLen = Math.max(maxLen, dp[i]);
             }
         }
         return maxLen;
