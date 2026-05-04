2c4144d9ea613c28f5c916884baba278873f1198
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2026-05-04 14:53:01 +0530

    Time: 4 ms (48.51%), Space: 45 MB (43.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,48 @@
 class Solution {
     public boolean isPalindrome(String s) {
-     int l=0;
-     int r=s.length()-1;
-     while(l<r)
-     {
-        while(l<r &&! Character.isLetterOrDigit(s.charAt(l)))
+    //  int l=0;
+    //  int r=s.length()-1;
+    //  while(l<r)
+    //  {
+    //     while(l<r &&! Character.isLetterOrDigit(s.charAt(l)))
+    //     {
+    //         l++;
+    //     }
+    //     while(l<r&&!Character.isLetterOrDigit(s.charAt(r)))
+    //     {
+    //         r--;
+    //     }
+    //     if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r)))
+    //     {
+    //         return false;
+    //     }
+    //     l++;
+    //     r--;
+    //  }   
+    //  return true;
+
+
+    StringBuilder str = new StringBuilder();
+    for(char ch:s.toCharArray())
+    {
+        if(Character.isLetterOrDigit(ch))
         {
-            l++;
+            str.append(Character.toLowerCase(ch));
         }
-        while(l<r&&!Character.isLetterOrDigit(s.charAt(r)))
-        {
-            r--;
-        }
-        if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r)))
-        {
-            return false;
-        }
-        l++;
-        r--;
-     }   
-     return true;
     }
-}
+    return CP(str.toString());
+    }
+        private boolean CP(String s)
+        {
+            int l=0,r=s.length()-1;
+            while(l<r)
+            {
+                if(s.charAt(l)!=s.charAt(r))
+                return false;
+                l++;
+                r--;
+            }
+            return true;
+        }
+    }
+    
