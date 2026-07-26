e7fb30aceff2c3657a985aaef381f82f69dca03f
Blessy-K <164822184+Blessy-K@users.noreply.github.com>
2026-07-26 10:52:23 +0530

    Time: 8 ms (46.2%), Space: 46.9 MB (84.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -11,15 +11,41 @@ class Solution {
     //     }
     //  }   
     //  return new int[]{};
-    HashMap<Integer,Integer> map = new HashMap<>();
+    // HashMap<Integer,Integer> map = new HashMap<>();
+    // for(int i=0;i<nums.length;i++)
+    // {
+    //     int ans = target-nums[i];
+    //     if(map.containsKey(ans))
+    //     {
+    //         return new int[]{map.get(ans),i};
+    //     }
+    //     map.put(nums[i],i);
+    // }
+    // return new int[]{};
+
+    int[][] arr = new int[nums.length][2];
     for(int i=0;i<nums.length;i++)
     {
-        int ans = target-nums[i];
-        if(map.containsKey(ans))
+        arr[i][0]=nums[i];
+        arr[i][1]=i;
+    }
+    Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+    int l=0,r=nums.length-1;
+    while(l<r)
+    {
+        int sum = arr[l][0]+arr[r][0];
+        if(sum==target)
         {
-            return new int[]{map.get(ans),i};
+            return new int[]{arr[l][1],arr[r][1]};
         }
-        map.put(nums[i],i);
+        else if(sum<target)
+        {
+            l++;
+        }
+        else
+        {
+            r--;
+        }
     }
     return new int[]{};
     }
