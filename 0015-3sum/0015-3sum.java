c4e957a4ca71d0c136d60a0165d550051736be53
Blessy <164822184+Blessy-K@users.noreply.github.com>
2026-07-29 17:59:05 +0530

    Time: 35 ms (43.96%), Space: 59.3 MB (45.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -19,22 +19,57 @@ class Solution {
     // return new ArrayList<>(set);
 
 
-    Set<List<Integer>> ans = new HashSet<>();
-    for(int i=0;i<nums.length;i++)
+    // Set<List<Integer>> ans = new HashSet<>();
+    // for(int i=0;i<nums.length;i++)
+    // {
+    //     HashSet<Integer> set = new HashSet<>();
+    //     for(int j=i+1;j<nums.length;j++)
+    //     {
+    //         int c = -(nums[i]+nums[j]);
+    //         if(set.contains(c))
+    //         {
+    //             List<Integer> temp = Arrays.asList(nums[i],nums[j],c);
+    //             Collections.sort(temp);
+    //             ans.add(temp);
+    //         }
+    //         set.add(nums[j]);
+    //     }
+    // }
+    // return new ArrayList<>(ans);
+
+
+    Arrays.sort(nums);
+    List<List<Integer>> ans = new ArrayList<>();
+    int n = nums.length;
+    for(int i=0;i<n-2;i++)
     {
-        HashSet<Integer> set = new HashSet<>();
-        for(int j=i+1;j<nums.length;j++)
+        if(i>0 && nums[i]==nums[i-1])
+        continue;
+        int l =i+1;
+        int r = n-1;
+        while(l<r)
         {
-            int c = -(nums[i]+nums[j]);
-            if(set.contains(c))
+            int sum = nums[i]+nums[l]+nums[r];
+            if(sum==0)
             {
-                List<Integer> temp = Arrays.asList(nums[i],nums[j],c);
-                Collections.sort(temp);
-                ans.add(temp);
+                ans.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                l++;
+                r--;
+                while(l<r && nums[l]==nums[l-1])
+                l++;
+                while(l<r && nums[r]==nums[r+1])
+                r--;
             }
-            set.add(nums[j]);
+            else if(sum<0)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
         }
     }
-    return new ArrayList<>(ans);
+    return ans;
     }
 }
