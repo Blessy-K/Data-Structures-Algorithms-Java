edc150c0f5adc18f9f11b7e7966743bf1e30d5e5
Blessy <164822184+Blessy-K@users.noreply.github.com>
2026-07-29 18:21:03 +0530

    Time: 1 ms (93.04%), Space: 47.4 MB (15.67%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,15 +1,34 @@
 class Solution {
     public int maxProduct(int[] nums) {
-    int maxi=Integer.MIN_VALUE;
+    // int maxi=Integer.MIN_VALUE;
+    // for(int i=0;i<nums.length;i++)
+    // {
+    //     int p=1;
+    //     for(int j=i;j<nums.length;j++)
+    //     {
+    //          p=p*nums[j];
+    //         maxi=Math.max(maxi,p);
+    //     }
+    // }  
+    // return maxi;  
+
+
+    int p=1,s=1;
+    int maxi = Integer.MIN_VALUE;
     for(int i=0;i<nums.length;i++)
     {
-        int p=1;
-        for(int j=i;j<nums.length;j++)
+        if(p==0)
         {
-             p=p*nums[j];
-            maxi=Math.max(maxi,p);
+            p=1;
         }
-    }  
-    return maxi;  
+        if(s==0)
+        {
+            s=1;
+        }
+    p=p*nums[i];
+    s=s*nums[nums.length-i-1];
+    maxi=Math.max(maxi,Math.max(p,s));
+    }
+    return maxi;
     }
 }
