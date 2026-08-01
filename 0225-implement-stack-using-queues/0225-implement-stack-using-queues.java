3f3ffd0e20aeb7731a86625fd989a64ba9d72893
Blessy <164822184+Blessy-K@users.noreply.github.com>
2026-08-01 18:17:58 +0530

    Time: 1 ms (73.37%), Space: 43.1 MB (5.4%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -2,36 +2,70 @@ import java.util.*;
 
 class MyStack {
 
-    Queue<Integer> q;
+Queue<Integer> q1;
+    Queue<Integer> q2;
 
     public MyStack() {
-        q = new LinkedList<>();
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
 
     public void push(int x) {
 
-        q.offer(x);
+        q2.offer(x);
 
-        int size = q.size();
-
-        while (size > 1) {
-            q.offer(q.poll());
-            size--;
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
         }
+
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
     }
 
     public int pop() {
-        return q.poll();
+        return q1.poll();
     }
 
     public int top() {
-        return q.peek();
+        return q1.peek();
     }
 
     public boolean empty() {
-        return q.isEmpty();
+        return q1.isEmpty();
     }
 }
+
+//     Queue<Integer> q;
+
+//     public MyStack() {
+//         q = new LinkedList<>();
+//     }
+
+//     public void push(int x) {
+
+//         q.offer(x);
+
+//         int size = q.size();
+
+//         while (size > 1) {
+//             q.offer(q.poll());
+//             size--;
+//         }
+//     }
+
+//     public int pop() {
+//         return q.poll();
+//     }
+
+//     public int top() {
+//         return q.peek();
+//     }
+
+//     public boolean empty() {
+//         return q.isEmpty();
+//     }
+// }
 
 /**
  * Your MyStack object will be instantiated and called as such:
