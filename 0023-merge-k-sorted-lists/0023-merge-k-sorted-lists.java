7238b18bac0cb6fe91a1954748f61651c03ccd03
Blessy <164822184+Blessy-K@users.noreply.github.com>
2026-08-04 18:32:30 +0530

    Time: 1 ms (99.93%), Space: 46.9 MB (46.66%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,27 +10,68 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-    PriorityQueue<ListNode> pq =
-                new PriorityQueue<>((a, b) -> a.val - b.val);
+    // PriorityQueue<ListNode> pq =
+    //             new PriorityQueue<>((a, b) -> a.val - b.val);
 
-        for (ListNode node : lists) {
-            if (node != null)
-                pq.offer(node);
-        }
+    //     for (ListNode node : lists) {
+    //         if (node != null)
+    //             pq.offer(node);
+    //     }
+
+    //     ListNode dummy = new ListNode(0);
+    //     ListNode tail = dummy;
+
+    //     while (!pq.isEmpty()) {
+
+    //         ListNode node = pq.poll();
+
+    //         tail.next = node;
+    //         tail = tail.next;
+
+    //         if (node.next != null)
+    //             pq.offer(node.next);
+    //     }
+
+    //     return dummy.next;
+
+    if (lists == null || lists.length == 0)
+            return null;
+
+        return divide(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divide(ListNode[] lists, int left, int right) {
+
+        if (left == right)
+            return lists[left];
+
+        int mid = left + (right - left) / 2;
+
+        ListNode l1 = divide(lists, left, mid);
+        ListNode l2 = divide(lists, mid + 1, right);
+
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode a, ListNode b) {
 
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode curr = dummy;
 
-        while (!pq.isEmpty()) {
+        while (a != null && b != null) {
 
-            ListNode node = pq.poll();
+            if (a.val <= b.val) {
+                curr.next = a;
+                a = a.next;
+            } else {
+                curr.next = b;
+                b = b.next;
+            }
 
-            tail.next = node;
-            tail = tail.next;
-
-            if (node.next != null)
-                pq.offer(node.next);
+            curr = curr.next;
         }
+
+        curr.next = (a != null) ? a : b;
 
         return dummy.next;    
     }
